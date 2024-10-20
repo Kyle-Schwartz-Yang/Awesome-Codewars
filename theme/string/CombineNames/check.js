af81ad2fb34a795ed44bafbd1ed9f663fab22ffe
schwartz <gtfsolo@gmail.com>
2024-10-20 19:32:20 +0300

    count of positives sum of negatives: 8kyu

--- theme/string/CombineNames/check.js
+++ theme/string/CombineNames/check.js
@@ -10,10 +10,3 @@ function combineNames (name, surname) {
 console.log(combineNames('James', 'Stevens'));
 
 
-
-function combine (name, surname) {
-  return name.concat(' ', surname);
-}
-
-
-console.log(combine('James', 'Stevens'));
