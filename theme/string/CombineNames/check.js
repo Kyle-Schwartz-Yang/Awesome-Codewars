df8146461ff054fdcf351630c91d72dd4133708e
schwartz <gtfsolo@gmail.com>
2024-10-20 17:14:24 +0300

    Combine Names :8kyu

---
+++ theme/string/CombineNames/check.js
@@ -0,0 +1,19 @@
+
+
+
+
+function combineNames (name, surname) {
+  return `${name} ${surname}`
+}
+
+
+console.log(combineNames('James', 'Stevens'));
+
+
+
+function combine (name, surname) {
+  return name.concat(' ', surname);
+}
+
+
+console.log(combine('James', 'Stevens'));
---
+++ theme/string/CombineNames/variant.md
@@ -0,0 +1,46 @@
+
+
+
+
+
+
+
+```js
+//  
+function combineNames (name, surname) {
+  return name.concat(' ', surname);
+}
+```
+
+```js
+//
+function combineNames (name, surname) {
+  return name + " " + surname;
+}
+```
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
