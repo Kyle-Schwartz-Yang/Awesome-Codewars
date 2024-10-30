32ff1f2a9e9e82dd1f4e342acae80a47e342deb6
schwartz <gtfsolo@gmail.com>
2024-10-30 22:02:59 +0200

    SquareSum and clear project

---
+++ theme/6kyu/Validate/createPhoneNumber/check.js
@@ -0,0 +1,14 @@
+
+
+
+/*Best Answer */
+function createPhoneNumber(numbers){
+  var format = "(xxx) xxx-xxxx";
+  
+  for(var i = 0; i < numbers.length; i++)
+  {
+    format = format.replace('x', numbers[i]);
+  }
+  
+  return format;
+}
